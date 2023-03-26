ff263001991706d77d4fc6fa96d840c7c7638f6e
Ramez Taher <ramezgamer11@gmail.com>
2023-03-26 15:04:34 +0100

    signup first done

---
+++ src/pages/FinishSignUp.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const FinishSignUp = () => {
+  return <div>FinishSignUp</div>
+}
+
+export default FinishSignUp
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,75 @@
-import React from 'react'
+import React from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { IoIosArrowBack } from "react-icons/io"
 
 const SignUp = () => {
+  const navigate = useNavigate()
+  const proceedToSignup = () => {
+    navigate("/auth/finish-register")
+  }
   return (
-    <div>SignUp</div>
+    <>
+      <section className="flex h-[100vh]">
+        <div className="w-[40%] h-full bg-auth-cover bg-center bg-cover flex items-center justify-center p-10">
+          <div className="text-white text-xl">
+            <div className="mb-4">
+              {" "}
+              “Develop a passion for learning. If you do, you will never cease
+              to grow.”
+            </div>
+            <div>Anthony J. D’Angelo</div>
+          </div>
+        </div>
+        <div className="w-[60%] h-full p-16 flex flex-col gap-16">
+          <div className="flex justify-between items-start">
+            <Link className="text-[#6D6D6D] flex items-center gap-1" to={"/"}>
+              <IoIosArrowBack />
+              Back
+            </Link>
+            <div className="flex flex-col text-right">
+              <div className="text-[#BDBDBD]">STEP 01/02</div>
+              <div className="text-[#909090]">Approving identity</div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-10 px-20 w-[80%]">
+            <div>
+              <h1 className="text-secondary text-3xl font-[700] mb-4">
+                Register Your Account
+              </h1>
+              <p className="text-grayish text-lg">
+                Your are invited to test our platform.
+              </p>
+            </div>
+            <form className="flex flex-col gap-8">
+              <label htmlFor="email">
+                Email adress*
+                <input
+                  type="text"
+                  name="email"
+                  placeholder="Enter your email adress"
+                />
+              </label>
+
+              <label htmlFor="code">
+                Invitation code*
+                <input
+                  type="text"
+                  name="code"
+                  placeholder="Enter your invitation code"
+                />
+              </label>
+
+              <button
+                className="btn btn-primary py-5 text-lg font-normal rounded-lg"
+                onClick={() => proceedToSignup()}
+              >
+                Proceed
+              </button>
+            </form>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
