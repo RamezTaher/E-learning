ffa1aa8c39eaf499b530cbc5a7c6eb8e9fe2dd4f
Ramez Taher <ramezgamer11@gmail.com>
2023-03-29 03:46:37 +0100

    gonna deal with the billing stuff

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import FinishSignUp from "./pages/FinishSignUp"
 import Home from "./pages/Home"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
+import UserBilling from "./pages/UserBilling"
 import UserCourseDetails from "./pages/UserCourseDetails"
 import UserCourses from "./pages/UserCourses"
 import UserDashboard from "./pages/UserDashboard"
@@ -18,6 +19,7 @@ function App() {
         <Route path="/platform/dashboard" element={<UserDashboard />} />
         <Route path="/platform/courses" element={<UserCourses />} />
         <Route path="/platform/courses/:id" element={<UserCourseDetails />} />
+        <Route path="/platform/billing" element={<UserBilling />} />
       </Routes>
     </>
   )
---
+++ src/pages/UserBilling.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const UserBilling = () => {
+  return <div>UserBilling</div>
+}
+
+export default UserBilling
