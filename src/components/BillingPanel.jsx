0211c478733b4178e7e3b5ee815ec8d8f5259c61
Ramez Taher <ramezgamer11@gmail.com>
2023-03-29 03:50:59 +0100

    gonna be okay for the last page xD

---
+++ src/components/BillingPanel.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import PlatformHeader from "./PlatformHeader"
+
+const BillingPanel = () => {
+  return (
+    <>
+      <div className="ml-[260px] w-full">
+        <div className="container px-6 sm:mx-auto lg:px-10 py-3 w-full">
+          <div className=" h-full w-full p-7 flex flex-col gap-10">
+            <PlatformHeader isSearch={false} location={"Billing"} />
+            <section className=""></section>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default BillingPanel
--- src/pages/UserBilling.jsx
+++ src/pages/UserBilling.jsx
@@ -1,7 +1,14 @@
 import React from "react"
+import BillingPanel from "../components/BillingPanel"
+import SideBar from "../components/SideBar"
 
 const UserBilling = () => {
-  return <div>UserBilling</div>
+  return (
+    <section className="flex">
+      <SideBar />
+      <BillingPanel />
+    </section>
+  )
 }
 
 export default UserBilling
